64a1f7322c2154f59f638a7e74c6b4c364fa9fe6
Mohak Bansal <mohak64bansal@gmail.com>
2023-01-21 12:09:44 +0530

    new commit 21 jan

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,6 +40,16 @@ export default function TextForm(props) {
     setText(event.target.value);
     
   }
+  const handleCopy = () => {
+    let text= document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
   const [text, setText] = useState('');
   return (
     
@@ -50,10 +60,12 @@ export default function TextForm(props) {
         
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="11"></textarea>
         </div>
+        <button className="btn btn-secondary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper Case</button>
         <button className="btn btn-danger mx-2" onClick={handleLoClick}>Convert to Lower Case</button>
         <button className="btn btn-dark mx-2" onClick={handleCaClick}>Capitalize first Char</button>
         <button className="btn btn-success mx-2" onClick={handleClClick}>Clear Text</button>
+        <button className="btn btn-warning mx-2" onClick={handleCopy}>Copy Text</button>
 
     </div>
     <div className="container my-4">
